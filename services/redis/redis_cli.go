1d2e6c7c00a44a9d7caf187debb8da8813362d54
GiveMeAcid <mazutichek@gmail.com>
2016-12-21 09:54:14 +0200

    updated redis client, added connectionPool

--- services/redis/redis_cli.go
+++ services/redis/redis_cli.go
@@ -1,17 +1,52 @@
 package redis
 
-import "github.com/garyburd/redigo/redis"
+import (
+	"github.com/garyburd/redigo/redis"
+	"time"
+	"log"
+)
 
 type RedisCli struct {
 	conn redis.Conn
 }
 
-func GetInstance() *RedisCli {
-	return
+var (
+	connectionPool  *redis.Pool = nil
+	ExpiredKeysChan = make(chan string)
+)
+
+func init() {
+	connectionPool = newPool()
 }
 
+func GetInstance() *RedisCli {
+	instanceRedisCli := new(RedisCli)
+	instanceRedisCli.conn = connectionPool.Get()
+	if err := instanceRedisCli.conn.Err(); err != nil {
+		log.Fatalln("Error connection to redis Server, error: ", err)
+	}
+	return instanceRedisCli
+}
+
+func newPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		//Dial:        dial,
+	}
+}
+
+//func dial() {
+//
+//}
+
 func (redisCli *RedisCli) SetValue(key string, value string, expiration ...interface{}) error {
-	return
+	_, err := redisCli.conn.Do("SET", key, value)
+
+	if err == nil && expiration != nil {
+		redisCli.conn.Do("EXPIRE", key, expiration[0])
+	}
+	return err
 }
 
 func (redisCli *RedisCli) GetValue(key string) (interface{}, error) {
