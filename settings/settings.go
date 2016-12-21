22451c55232cb0cf69342b26e9cdf518a8f8effc
GiveMeAcid <mazutichek@gmail.com>
2016-12-21 10:19:24 +0200

    added settings

--- settings/settings.go
+++ settings/settings.go
@@ -1,9 +1,46 @@
 package settings
 
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+)
+
+var environments = map[string]string{
+	"production":    "settings/prod.json",
+	"preproduction": "settings/pre.json",
+	//"tests":         "../../settings/tests.json",
+}
+
 type Settings struct {
 	PrivateKeyPath     string
 	PublicKeyPath      string
 	JWTExpirationDelta int
+}
+
+var settings Settings = Settings{}
+var env = "preproduction"
+
+func Init() {
+	env = os.Getenv("GO_ENV")
+	if env == "" {
+		fmt.Println("Warning: Setting preproduction environment due to lack of GO_ENV value")
+		env = "preproduction"
+	}
+	LoadEnvironmentalSettings(env)
+}
+
+func LoadEnvironmentalSettings(env string) {
+	content, err := ioutil.ReadFile(environments[env])
+	if err != nil {
+		fmt.Println("Error while reading config file", err)
+	}
+	settings = Settings{}
+	jsonErr := json.Unmarshal(content, &settings)
+	if jsonErr != nil {
+		fmt.Println("Error while parsing config file")
+	}
 }
 
 func Get() Settings {
